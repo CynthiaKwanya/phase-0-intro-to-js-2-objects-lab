9fb0391dab0c4cfa588957686a8091aafad1f323
Cynthia Kwanya Akinyi <kwanyac15@gmail.com>
2023-03-17 17:12:44 +0300

    object lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your solution in this file!
+// Write your solutionee in this file!
+const employee = {
+    name: "John Doe",
+    streetAddress: "123 Main St"
+  };
+
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+      ...employee,
+      [key]: value
+    };
+  }
+
+  
+let Employee = {
+  name: "John",
+  age: 30,
+  title: "Manager"
+};
+
+function deleteFromEmployeeByKey(employee, key) {
+    const { [key]: deletedProp, ...rest } = employee;
+    return rest;
+  }
+  
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
+  
